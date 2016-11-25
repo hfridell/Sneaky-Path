e8ed5f143121c6598b4199c360379f7a07e28b98
Hank.Fridell <hf.fridell@gmail.com>
2016-11-25 12:28:05 -0600

    .

--- src/edu/umkc/Graph.java
+++ src/edu/umkc/Graph.java
@@ -31,7 +31,8 @@ public class Graph {
     this.flowMatrix = flowMatrix;
   }
 
-  public void floydWarshal() {
+  public void floydWarshall() {
+    // https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm#Path_reconstruction
     // Build matrix for path reconstruction
     nextMatrix = new ArrayList<>(size);
     for (Integer i = 0; i < size; i++) {
@@ -42,7 +43,7 @@ public class Graph {
     }
 
     //initializeAllPairsShortestPath();
-    // Floyd Warshal
+    // Floyd Warshall
     for (int k = 0; k < size; k++) {
       for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
